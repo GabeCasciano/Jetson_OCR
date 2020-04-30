d2ae6c78985e446d3ee9624d09934e9c1cbce213
unknown <gabecasciano@gmail.com>
2020-04-29 17:24:10 -0700

    Working

--- learning/deep_ocr.py
+++ learning/deep_ocr.py
@@ -1,13 +1,6 @@
 from PIL import Image
 from tesserocr import PyTessBaseAPI
 
-def toBW(img):
-    column = Image.open(img)
-    gray = column.convert('L')
-    bw = gray.point(lambda x: 0 if x < 200 else 255, '1')
-    bw.save("bw_im.jpg")
-    return "bw_im.jpg"
-
 def ocr(img):
     with PyTessBaseAPI () as api:
         api.SetImageFile(img)
